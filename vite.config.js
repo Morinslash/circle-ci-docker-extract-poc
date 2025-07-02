6f88a5c5f95c120ecaf7470ee78787be02baab35
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2025-07-02 15:49:18 +0200

    fix typo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cicle-ci-docker-extract-poc/',
+  base: '/circle-ci-docker-extract-poc/',
   plugins: [react()],
 })
